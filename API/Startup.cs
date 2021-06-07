6d8965210c60fe85f125b3c6a09eed760f099287
Mateusz Zajac <TRBC37@motorolasolutions.com>
2021-06-07 20:08:22 +0200

    add client app initial stage

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,6 +38,11 @@ namespace API
             services.AddDbContext<DataContext>(opt=>{
                 opt.UseSqlite(_config.GetConnectionString("DevConnection"));
             });
+            services.AddCors(x=> {
+                x.AddPolicy("CorsPolicy", policy=>{
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,10 +54,11 @@ namespace API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
-
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
